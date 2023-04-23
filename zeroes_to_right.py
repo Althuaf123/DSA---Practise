0e3de538b9db0179474b0e00fbd81ab7ca1ef449
Althuaf <althuafalthaf@gmail.com>
2023-04-23 13:34:42 +0530

    Day-5

---
+++ zeroes_to_right.py
@@ -0,0 +1,9 @@
+def move_zeroes(arr):
+    j = 0
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[i],arr[j] = arr[j],arr[i]
+            j += 1
+    return arr
+
+print(move_zeroes([0,1,0,0,2,0,3,0,0,4,5,0]))
